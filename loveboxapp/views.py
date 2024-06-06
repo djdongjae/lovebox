53e42d12c7c08a2eb0da8b3c8ee13479ef9da928
djdongjae <jk47wee@gmail.com>
2024-06-06 21:51:56 +0900

    refactor: 질문 및 MBTI 선택지 추가

--- loveboxapp/views.py
+++ loveboxapp/views.py
@@ -23,6 +23,19 @@ def create(request):
     form.mbti = request.POST['mbti']
     form.price = request.POST['price']
     form.fashion_style = request.POST['fashion_style']
+    form.hobby = request.POST['hobby']
+    form.color = request.POST['color']
+    form.food = request.POST['food']
+    form.character = request.POST['character']
+    form.laugh_cause = request.POST['laugh_cause']
+    form.reaction_to_new_experience = request.POST['reaction_to_new_experience']
+    form.music_style = request.POST['music_style']
+    form.weather = request.POST['weather']
+    form.sns_style = request.POST['sns_style']
+    form.travel = request.POST['travel']
+    form.movie_genre = request.POST['movie_genre']
+    form.cat_or_dog = request.POST['cat_or_dog']
+    form.value = request.POST['value']
     form.save()
     return redirect(f'/result?form_id={form.id}')
 
@@ -37,14 +50,41 @@ def result(request):
     questionList = [
         "연인의 성별은 어떻게 되나요?",
         "연인의 MBTI가 무엇인가요?",
-        "예상 선물 가격대는?",
-        "연인이 선호하는 패션 스타일은?"
+        "예상 선물 가격대는?(단위는 달러)",
+        "연인이 선호하는 패션 스타일은?",
+        "연인의 취미는 어떤 부류인가요?",
+        "연인이 좋아하는 색깔은?",
+        "연인이 좋아하는 음식은?",
+        "연인의 정치 성향은?",
+        "연인의 행복 모먼트는?",
+        "새로운 경험을 할 때 보이는 반응",
+        "좋아하는 음악 스타일은 무엇인가요?",
+        "연인이 좋아하는 날씨는 어떤가요?",
+        "연인의 SNS 사용 빈도는?",
+        "연인은 어떤 여행을 선호하나요?",
+        "선호하는 장르는 무엇인가요?",
+        "강아지 vs 고양이",
+        "연인의 중요한 가치관은 무엇인가요?"
     ]
     
     reqText += "\ninput: " + questionList[0] + "\noutput: " + form.sex
     reqText += "\ninput: " + questionList[1] + "\noutput: " + form.mbti
     reqText += "\ninput: " + questionList[2] + "\noutput: " + str(form.price)
     reqText += "\ninput: " + questionList[3] + "\noutput: " + form.fashion_style
+    reqText += "\ninput: " + questionList[4] + "\noutput: " + form.hobby
+    reqText += "\ninput: " + questionList[5] + "\noutput: " + form.color
+    reqText += "\ninput: " + questionList[6] + "\noutput: " + form.food
+    reqText += "\ninput: " + questionList[7] + "\noutput: " + form.character
+    reqText += "\ninput: " + questionList[8] + "\noutput: " + form.laugh_cause
+    reqText += "\ninput: " + questionList[9] + "\noutput: " + form.reaction_to_new_experience
+    reqText += "\ninput: " + questionList[10] + "\noutput: " + form.music_style
+    reqText += "\ninput: " + questionList[11] + "\noutput: " + form.weather
+    reqText += "\ninput: " + questionList[12] + "\noutput: " + form.sns_style
+    reqText += "\ninput: " + questionList[13] + "\noutput: " + form.travel
+    reqText += "\ninput: " + questionList[14] + "\noutput: " + form.movie_genre
+    reqText += "\ninput: " + questionList[15] + "\noutput: " + form.cat_or_dog
+    reqText += "\ninput: " + questionList[16] + "\noutput: " + form.value
+
         
     reqText += "\n\ninput: 연인에게 선물할 물건명 한가지와 추천한 선물에 대한 설명을 제공한다.\n" + "output:\n"
     
--- loveboxprj/settings.py
+++ loveboxprj/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'loveboxapp.apps.LoveboxappConfig'
+    'loveboxapp.apps.LoveboxappConfig',
 ]
 
 MIDDLEWARE = [
